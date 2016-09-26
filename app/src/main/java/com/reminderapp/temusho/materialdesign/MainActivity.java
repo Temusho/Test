e2b3ee0d94db1920443b6dbe3058d7c7a7d50863
Temusho <temobrilly@gmail.com>
2016-09-26 10:37:52 +0200

    up ..up ..up ..away

---
+++ app/src/main/java/com/reminderapp/temusho/materialdesign/MainActivity.java
@@ -0,0 +1,65 @@
+package com.reminderapp.temusho.materialdesign;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar my_toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(my_toolbar); //set up the compatibility with old android versions
+
+        getSupportActionBar().setTitle(R.string.my_tb_title);
+        getSupportActionBar().setSubtitle(R.string.my_tb_subtitle);
+        getSupportActionBar().setIcon(R.drawable.ic_action_name);
+
+        Intent searchIntent = getIntent();
+        if(Intent.ACTION_SEARCH.equals(searchIntent.getAction())){
+            String query = searchIntent.getStringExtra(SearchManager.QUERY);
+            Toast.makeText(this, query, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        SearchView searchView = (SearchView) menu.getItem(R.id.menu_search).getActionView();
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+       /* int id = item.getItemId();
+
+        if(id == R.id.my_menu){}*/
+
+        switch (item.getItemId()){
+            case R.id.my_menu:
+                Toast.makeText(this, "Its her name" , Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.option_2:
+                Toast.makeText(this, "Its her surname" , Toast.LENGTH_LONG).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
